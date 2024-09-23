1f24253b3606c3134caf139e1af75105951c4b21
Jakub Mamala <G00408184@atu.ie>
2024-09-23 12:29:36 +0100

    created the week 2 lab inside intellij

---
+++ src/test/java/ie/atu/week2_refreshers/Week2RefreshersApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week2_refreshers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2RefreshersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
